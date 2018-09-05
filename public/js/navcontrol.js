a3dc3afe8e6391eaf4756ff538060a6f8b01b355
Lamoliveira <LamOliveira@gmail.com>
2018-09-04 20:19:44 -0400

    menuhide

--- public/js/navcontrol.js
+++ public/js/navcontrol.js
@@ -6,16 +6,39 @@ $(document).ready(function () {
     console.log("isconnected:"+$(document).isConnected);
     
     console.log("title:"+ $(document).context.title);
+    $("#menutournaments").hide();
+
 
     $.get("/api/user_data").then(function (data) {
         console.log("email:" + data.email);
-        //$("#menutournaments").hide;
-
+        
         if (data.email) {
             $("#menulogin").text("logged as:" + data.email);
+            
+            //$("#menurule").style.display = 'block';
+   
+            document.getElementById("menurule").style.display = 'block';
+            document.getElementById("menublog").style.display = 'block';
+            document.getElementById("menuauthors").style.display = 'block';
+            document.getElementById("menuaddtours").style.display = 'block';
+            document.getElementById("menutours").style.display = 'block';
+            document.getElementById("menuleagues").style.display = 'block';
+            document.getElementById("menutrain").style.display = 'block';
+            document.getElementById("menulogout").style.display = 'block';
+            document.getElementById("menusignup").style.display = 'none';
         } 
         else {
             console.log("notlogged");
+            document.getElementById("menurule").style.display = "none";
+            document.getElementById("menublog").style.display = 'none';
+            document.getElementById("menuauthors").style.display = 'none';
+            document.getElementById("menuaddtours").style.display = 'none';
+            document.getElementById("menutours").style.display = 'none';
+            document.getElementById("menuleagues").style.display = 'none';
+            document.getElementById("menutrain").style.display = 'none';
+            document.getElementById("menulogout").style.display = 'none';
+            document.getElementById("menusignup").style.display = 'block';
+              
           //  window.location.href = "/tours";
         }
     });
